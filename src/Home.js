c62326e33fbf5b4fc27a453332b102142ccf429b
windows <windows@microsoft.com>
2022-11-17 13:04:51 +0530

    my create new

---
+++ src/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+function Myhome()
+{
+    return <h1>welcome react page using react</h1>
+}
+
+export default Myhome
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Myhome from './Home';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Myhome></Myhome>
+  </React.StrictMode>
+);
